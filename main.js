bb6d3f4eeba5207585e87dc7bcfb5f8c783dfa81
Sebastian Rogiński <sebastian.roginski@onet.pl>
2022-11-21 10:57:21 +0100

    zmiana koloru i wielkości tesktu

--- main.js
+++ main.js
@@ -1,33 +1,40 @@
-const arrowBtn = document.querySelector('.arrow')
-const arrowIcon = document.querySelector('.fas')
-const img = document.querySelector('.item1')
-
-// nasłuchiwanie na przycisk
-
+const sizeUp = document.querySelector('.sizeUp')
+const sizeDown = document.querySelector('.sizeDown')
+const colorBtn = document.querySelector('.color')
+const p = document.querySelector('p')
 
 
-
-// dodawanie klasy hide
-
-
-
-//obracanie strzałki
-
-
-
-
-const showImg = () => {
-    img.classList.toggle('hide')
-
-
-if(img.classList.contains('hide')){
-    arrowIcon.style.transform = 'rotate(180deg'
-} else {
-    arrowIcon.style.transform = 'rotate(0)'
-}
+let fontSize = 36
+const powiekszenie = () => {
+    fontSize += 3
+    p.style.fontSize = fontSize + 'px'
+   
 }
 
-arrowBtn.addEventListener('click', showImg)
+
+
+const pomniejszenie = () => {
+    fontSize -= 3
+    if(fontSize <=12){
+        return
+    }
+
+    p.style.fontSize = fontSize + 'px'
+}   
+
+
+
+const zmianaKoloru = () => {
+   const r = Math.floor(Math.random(0)* 255)
+   const g = Math.floor(Math.random(0)* 255)
+   const b = Math.floor(Math.random(0)* 255)
+
+   p.style.color = `rgb(${r}, ${g}, ${b})`
+}
+
+colorBtn.addEventListener('click', zmianaKoloru)
+sizeUp.addEventListener('click', powiekszenie)
+sizeDown.addEventListener('click', pomniejszenie)
 
 
 
